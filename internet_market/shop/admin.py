476c68959df0e5a77f52657a3eaaa70be71ef8e5
Artem <artem@MacBook-Air-Artem.local>
2025-09-21 04:55:39 -0500

    mark best version

--- internet_market/shop/admin.py
+++ internet_market/shop/admin.py
@@ -1,16 +1,18 @@
+# shop/admin.py
 from django.contrib import admin
 from .models import Category, Product
 
-# Register your models here.
 @admin.register(Category)
-class Category(admin.ModelAdmin):
-    list_display = ('id', 'slug', 'name_ru', 'name_ua', 'name_en')
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'slug', 'name_ua', 'name_ru', 'name_en')
     list_display_links = ('id', 'slug')
+    search_fields = ('name_ua', 'name_ru', 'name_en')
+
 
 @admin.register(Product)
-class Product(admin.ModelAdmin):
-    list_display = ('id', 'title', 'slug', 'short_description', 'created_at')
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'slug', 'created_at')
     list_display_links = ('id', 'title', 'slug')
     search_fields = ('title', 'short_description')
-    list_filter = ('created_at',)
-    
+    list_filter = ('created_at', 'category')
+
--- internet_market/shop/models.py
+++ internet_market/shop/models.py
@@ -1,30 +1,40 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
     id = models.AutoField(primary_key=True)
     slug = models.SlugField((""))
-    name_ru = models.CharField(max_length=100)
-    name_ua = models.CharField(max_length=100)
-    name_en = models.CharField(max_length=100)
+    name_ru = models.CharField("Name in Russian", max_length=100)
+    name_ua = models.CharField("Name in Ukrainian", max_length=100)
+    name_en = models.CharField("Name in English", max_length=100)
     
 # class Color:
 #     name = models.CharField(50)
 #     hex_color = models.CharField(7 , default="#00000")
 
     def __str__(self):
-        return self.name
-    
-
+        return self.name_ru
 
 class Product(models.Model):
     id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200)
+    title = models.CharField("Title")
     slug = models.SlugField(max_length=200, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     short_description = models.TextField()
     long_description = models.TextField()
     # palitre = models.ManyToManyField(Color, blank=True, related_name="products")
+    main_image = models.ImageField(upload_to='products/', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        ordering = ['-created_at']
+        
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("product_detail", kwargs={"slug": self.slug})
+    
+
+    
