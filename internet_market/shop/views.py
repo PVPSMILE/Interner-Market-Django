7d1f3ab174a6ea07f8537f17fd2faa2767646256
Artem <artem@MacBook-Air-Artem.local>
2025-11-02 05:35:18 -0600

    02.11.2025

--- internet_market/shop/views.py
+++ internet_market/shop/views.py
@@ -4,7 +4,8 @@ from .models import Category, Product
 # Create your views here.
 def shop_page(request):
     products = Product.objects.all()
-    return render(request, 'shop/shop.html', context={'products': products})
+    categories = Category.objects.all()
+    return render(request, 'shop/shop.html', context={'products': products, 'categories': categories})
 
 def product_page(request, slug):
     
